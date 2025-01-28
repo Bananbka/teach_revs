7301323a8575ea7997eaf6c8593c7b10be3590a1
Bananbka <bananbka0.2@gmail.com>
2025-01-28 18:18:20 +0200

    Add Hash to SHA256 function and Salt Generation function

--- teacher-reviews/src/main/java/org/example/teacherreviews/TeacherReviewsApplication.java
+++ teacher-reviews/src/main/java/org/example/teacherreviews/TeacherReviewsApplication.java
@@ -8,26 +8,58 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Iterator;
 
 @SpringBootApplication
 public class TeacherReviewsApplication {
 
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(TeacherReviewsApplication.class, args);
-
+	public static void getUniversitiesFromAPI() throws IOException {
 		URL url = new URL("https://registry.edbo.gov.ua/api/universities/?exp=json");
 
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode json = mapper.readTree(url);
-        for (Iterator<JsonNode> it = json.elements(); it.hasNext(); ) {
+		for (Iterator<JsonNode> it = json.elements(); it.hasNext(); ) {
 			JsonNode i = it.next();
 			University uni = new University(i);
 			System.out.println(uni);
-
 		}
-
-
-		System.out.println("Hello, world! st: ");
 	}
-}
+
+	public static void generateSalt() {
+		SecureRandom secureRandom = new SecureRandom();
+		byte[] salt = new byte[16];
+		secureRandom.nextBytes(salt);
+		String saltyString = Base64.getEncoder().encodeToString(salt); //result
+	}
+
+	private static String toSHA256(String toEncode) throws NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		byte[] hash = digest.digest(
+				toEncode.getBytes(StandardCharsets.UTF_8));
+
+		StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+		return hexString.toString();
+	}
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		SpringApplication.run(TeacherReviewsApplication.class, args);
+
+		String coolPassword = "qwerty83";
+		System.out.println("My password:\n" + coolPassword + "\nHashed:\n" + toSHA256(coolPassword));
+
+
+		System.out.println("Finishing...");
+	}
+}	
