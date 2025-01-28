f75c1e3207845bb0cb6fb3c8402d2e99c9872b71
Bananbka <bananbka0.2@gmail.com>
2025-01-28 23:14:00 +0200

    WIP: Add Python script running from Java code

--- teacher-reviews/src/main/java/org/example/teacherreviews/TeacherReviewsApplication.java
+++ teacher-reviews/src/main/java/org/example/teacherreviews/TeacherReviewsApplication.java
@@ -3,10 +3,12 @@ package org.example.teacherreviews;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.teacherreviews.DTO.University;
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
+import javax.script.*;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -14,6 +16,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootApplication
 public class TeacherReviewsApplication {
@@ -53,13 +58,33 @@ public class TeacherReviewsApplication {
 		return hexString.toString();
 	}
 
-	public static void main(String[] args) throws NoSuchAlgorithmException {
+	private static String resolvePythonScriptPath(String path){
+		File file = new File(path);
+		return file.getAbsolutePath();
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TeacherReviewsApplication.class, args);
 
-		String coolPassword = "qwerty83";
-		System.out.println("My password:\n" + coolPassword + "\nHashed:\n" + toSHA256(coolPassword));
+		String workingDir = System.getProperty("user.dir");
+		System.out.println("Current working directory: " + workingDir);
+
+		ProcessBuilder processBuilder = new ProcessBuilder("python3", resolvePythonScriptPath(resolvePythonScriptPath("py-modules/frequency_sensitivity_detector.py")));
+		processBuilder.redirectErrorStream(true);
+
+		Process process = processBuilder.start();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println("Python output: " + line); // Виводимо результат
+			}
+		}
+
+		// Чекаємо завершення процесу
+		int exitCode = process.waitFor();
+		System.out.println("Python script exited with code: " + exitCode);
 
 
 		System.out.println("Finishing...");
 	}
-}	
+}
